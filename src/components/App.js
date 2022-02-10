d5ca08382b491a39995fd407019f3186d144afb8
JunyoungLee0421 <63839551+JunyoungLee0421@users.noreply.github.com>
2022-02-10 19:36:43 +0900

    updated : delete/update methods added

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React, {useEffect, useState} from 'react';
 import AppRouter from 'components/Router';
 import {authService} from "fBase";
 
+//유저 로그인유무가 제일 위 (app.js) 에 있어야하고
+//그 후로 app.js => router.js로 
+// router.js => 스크린(home.js, profile.js, nweet.js 등) 으로 보냄
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -17,7 +23,12 @@ function App() {
   }, []);
   return (
   <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+    {init ? 
+    (
+      <AppRouter isLoggedIn={isLoggedIn} userObj = {userObj} /> 
+    ) : ( 
+      "Initializing..." 
+      )}
     <footer>
       &copy; {new Date().getFullYear()} Nwitter
     </footer>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,70 @@
-import React from "react";
+import Nweet from "components/Nweet";
+import { dbService } from "fBase";
+import { addDoc, collection, doc, getDocs, query, onSnapshot, orderBy } from "firebase/firestore";
+import React, {useEffect, useState}  from "react";
 
-const Home = () => <span>Home</span>;
+const Home = ({userObj}) => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    /* old way 
+    const getNweets = async() => {
+        const dbnweets = await getDocs(collection(dbService, "nweets"));
+        dbnweets.forEach(document => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    */
+    //new way => it doens'nt rerender (render only once)
+    useEffect(() => {
+        const q = query(collection(dbService, "nweets"), orderBy("createdAt", "desc"));
+        //데이터베이스에 변화가 있을때마다 호출됨
+        onSnapshot(q, (snapshot) => {
+            const nweetArray = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setNweets(nweetArray);
+        })
+    }, []);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), {
+                text: nweet,
+                createdAt: Date.now(),
+                createrId: userObj.uid,
+            }); 
+            console.log("document written with Id : ", docRef.id);
+        } catch(error) {
+            console.log("error adding docuemnt : ", error);
+        }
+
+        setNweet("");
+    };
+
+    const onChange = (event) => {
+        const {target:{value},} = event;
+        setNweet(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value = {nweet} onChange = {onChange} type = "text" placeholder="Waht's on your mind?" maxLength={120} />
+                <input type = "submit" value = "Nweet" />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet key = {nweet.id} nweetObj = {nweet} isOwner = {nweet.createrId === userObj.uid}/>
+                ))}
+            </div>
+        </div>
+        );
+    };
 
 export default Home;
